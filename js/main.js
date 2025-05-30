70b6129cff998f619db60190cee95bcee3a9ae9f
Mohammed Fachihul Alam <fachirbl@gmail.com>
2025-05-31 00:15:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".staff-list").owlCarousel({
+        items: 4,
+        autoplay: false,
+        margin: 30,
+        nav: true,
+        navText: [
+            "<i class='fas fa-arrow-left'></i>",
+            "<i class='fas fa-arrow-right'></i>",
+        ],
+    });
+});
