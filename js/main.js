777e9ebc2e489822a63a866a88ba13de8e120c6e
Fachihul <fachirbl@gmail.com>
2025-05-31 22:23:43 +0600

    f

--- js/main.js
+++ js/main.js
@@ -8,5 +8,26 @@ $(document).ready(function () {
             "<i class='fas fa-arrow-left'></i>",
             "<i class='fas fa-arrow-right'></i>",
         ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+            },
+            600: {
+                items: 2,
+                nav: false,
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false,
+            },
+            1200: {
+                items: 4,
+                nav: true,
+                loop: false,
+            },
+        },
     });
 });
